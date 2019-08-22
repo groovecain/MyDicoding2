7a21605636fc02121e959c1b6454471c7f841304
STEVEN (IT-PC) <steve.mailme@gmail.com>
2019-08-22 09:50:56 +0700

    Add data array resep

--- app/src/main/java/com/example/mydicoding/ResepData.kt
+++ app/src/main/java/com/example/mydicoding/ResepData.kt
@@ -3,7 +3,7 @@ package com.example.mydicoding
 object ResepData {
     private val data = arrayOf(
         arrayOf(
-            "Ayam goreng kecap",
+            "Ayam Goreng Kecap",
             "infokuliner.com",
             "Ayam goreng kecap merupakan sajian ayam yang gurih dan lembut. ",
             "- 500gr Daging ayam, dipotong beberapa bagian \n- 6sdm Kecap Manis \n- 3 butir bawang merah, di iris \n- 100ml Air \n- 1/2sdt merica \n- 2 buah jeruk nipis \n- 3 siung bawang putih, di iris \n- 1/2sdt garam \n- Minyak goreng secukupnya",
@@ -25,6 +25,30 @@ object ResepData {
             "- Tempe 500gr \n- Lengkuas 2 siung (kupas kulit-nya) \n- 1 Lembar daun salam \n- Kecap manis secukupnya \n- Gula secukupnya \n- Garam secukupnya \n- Penyedap rasa secukupnya \n- Minyak goreng secukupnya",
             "1. Potong kecil-kecil tempe berbentuk dadu. \n2. Tumis bawang putih dengan minyak goreng sampai harum. Masukkan daun salam dan lengkuas, lalu tumis kembali sampai berubah warna. \n3. Setelah matang, masukkan tempe, gula, garam, penyedap rasa, dan kecap. Tumis hingga tercampur merata. \n4. Tambahkan air secukupnya, lalu tunggu sampai matang. \n5. Orek tempe pun siap dihidangkan.",
             "https://cdn.idntimes.com/content-images/post/20181123/3-e86b4f247d1a1bdca696b87001da55d7.jpg"
+        ),
+        arrayOf(
+            "Nasi Uduk",
+            "selerasa.com",
+            "Sajikan dan nikmati lezatnya resep nasi sebagai menu bahan utama dari hidangan kali ini.",
+            "- 600gr beras \n- 2 1/2 Sendok teh garam \n- 3 Batang serai \n- Santan dari 1 butir kelapa \n- 30 Lembar daun jeruk, buang tulang daunnya & iris tipis \n- 4 Lembar daun pandan \n- 24 Lembar daun salam \n- Bawang merah goreng untuk tabugan",
+            "1. Untuk membuat hidangan nasi uduk kali ini anda akan dapat melakukannya dengan terlebih dahulu mendidihkan santan didalam sebuah panci. Lalu letakkan diatas kompor dengan menggunakan api yang sedang. \n2. Kemudian masukkan kedalamnya garam bersama dengan serai, daun pandan dan juga daun jeruk. Aduk-aduk semua bahan ini secara merata sampai tercampur. \n3. Selanjutnya baru anda bisa memasukkan beras kedalamnya dan aduk sampai santan meresap kedalamnya. Lalu sisihkan sajian ini dengan merata. \n4. Kemudian panaskan kukusan diatas api yang sedang dan tunggu sampai panas. Baru anda bisa masukkan beras yang sudah dimasak setengah matang kedalamnya. \n5. Kukus beras selama kurang lebih 30 menit sampai matang dan merata. Mnejelang diangkat anda akan dapat memasukkan daun jeruk kedalamnya. \n6. Nah, setelah sajian ini matang maka anda akan dapat menyajikannya diatas piring bersama lauk tambahan seperti ayam goreng. Hidangan ini akan dapat anda jadikan sebagai santapan untuk sarapan dan dinikmati bersama dengan kerupuk, sambal dan lalaban yang enak dan segar.",
+            "https://cdn1-production-images-kly.akamaized.net/O6mcDdRamNkW7XRF0CM-zjPuofc=/640x360/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/1557598/original/043630500_1491389074-shutterstock_478336309.jpg"
+        ),
+        arrayOf(
+            "Cumi Asin Pete",
+            "resepmasakan.id",
+            "Jenis makanan yang satu ini tidak hanya nikmat dimulut tapi juga sangat sarat dengan kandungan gizi yang tinggi",
+            "- 200 gr cumi asin \n- 1 papan pete belah menjadi 2 biji \n- 10 bh cabe ijo potong serong \n- 3 buah cabe merah potong serong \n- 5 buah rawit merah potong serong \n- 5 buah rawit hijau potong serong \n- 5 buah bawang merah iris tipis \n- 3 buah bawang putih iris tipis \n- 1/2 buah bawang bombay \n- Sedikit tomat iris \n- 1 ruas lengkoas keprek \n- Sedikit jahe keprek \n- 2 lembar daun salam \n- 2 lembar daun jeruk \n- 1 buah sereh kecil keprek \n- 1 sendok makan kecap manis \n- 1 sendok makan saos tiram \n- Sedikit garam dan kaldu ayam bubuk \n- 50 ml air \n- Minyak goreng secukupnya",
+            "1. Didihkan air ksh garam dikit lalu masukan cumi asin yang sudah di cuci bersih sampai cumi tersebut mengembang angkat dan tiriskan \n2. Potong-potong cumi dan Buang tulang putih cumi sm isi perutnya goreng betar tiriskan \n3. Panaskan minyak goreng lalu tumis bawang merah dan bawang putih sampai harum lalu masukan cabe2an, tomat,daun salam, daun jeruk, sereh,jahe dan lengkoas aduk sampai layu \n4. Lalu masukan cumi, pete dan bawang bombay aduk bentar lalu masukan kecap manis, saos tiram, sedikit garam, sedikit bubuk kaldu ayam dan air aduk dan diamkan sebentar \n5. Aduk lagi lalu tes rasa kalau sudah ok semua angkat dan sajikan",
+            "https://resepmasakan.id/wp-content/uploads/2019/01/Resep-Cumi-Asin-Pete.jpg"
+        ),
+        arrayOf(
+            "Mie Rebus Minang",
+            "www.masakapahariini.com",
+            "Kekhasan Mie Rebus Minang adalah kuah berwarna merah berkat kehadiran cabai ulek dan kuah kaldu tulang sumsum sapi.",
+            "- 140g mi telur \n- 4 sdm kol iris \n- 1 sdm cabai merah giling \n- 1 sdm kecap asin \n- 1 sdt cuka \n- 2 sdm minyak \n- 600ml air \n- 2 siung bawang putih \n- 1 sdm daun bawang iris \n- 1 sdt Royco kaldu sapi \n- 1 sdm Bango kecap manis \n- 2 butir telur \n- 400ml kaldu tulang sumsum sapi",
+            "1. Rebus mie dengan air mendidih selama 2-3 menit. Angkat dan tiriskan. \n2. Panaskan 2 sdm minyak, tumis bawang putih dan cabai hingga harum. Masukkan irisan kol, daun bawang, Royco Kaldu Sapi, kecap asin, dan Bango Kecap Manis. \n3. Tuang kaldu sapi dan 2 butir telur yang sudah dikocok lepas, aduk rata. Masukkan mie, aduk. Masak hingga matang. Angkat. \n4. Sajikan mie rebus dengan telur mata sapi dan rendang sapi.",
+            "https://www.masakapahariini.com/wp-content/uploads/2019/03/mie--400x240.jpg"
         )
     )
 
